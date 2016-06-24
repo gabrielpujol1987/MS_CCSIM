24ac506e26ac7413884fca9065b09c37096b3348
Gabriel Santiago Pujol Fariña <gabrielpujol87@gmail.com>
2016-06-24 18:10:39 -0300

    Putting a decision in the MinCut
    - the 'mincut_func.m' got a decision global variable  "global
    use_mincut".
    - Added the PrintImage file, to print an image...  needs some more work.
    - in 'runme.m' fixed a normalization bug, and some parameters for the
    new experiments.

--- runme.m
+++ runme.m
@@ -16,29 +16,29 @@
 
 
 %% 2D S I M U L A T I O N E X A M P L E
-
 clc
 clear all
 close all
 
+global use_mincut;
 
 % Input Parameters
 % % % load ti_2D_1; % Two examples are avilable: ti_2D_1 and ti_2D_2
 
-% ti = read_eas_sq('Bangladesh.sgems');
-% ti = read_eas_sq('tis\ti_strebelle.sgems');
+% ti = read_eas_sq('tis\Bangladesh.sgems');
+ti = read_eas_sq('tis\ti_strebelle.sgems');
 
 % ti = read_eas_sq('tis\C_WLTICAT.sgems');
-ti = read_eas_sq('tis\A_WLreferenceCAT.sgems');
+% ti = read_eas_sq('tis\A_WLreferenceCAT.sgems');
 
-nx_sim = 400;
-ny_sim = 400;
+nx_sim = 256;
+ny_sim = 256;
 
 % hd = NaN(1000); % Simulation grid (1000x1000)
 hd = NaN(nx_sim, ny_sim); % Simulation grid (1000x1000)
 
 CT = [2 2];			% Co-Template size in 'i' and 'j' directions
-OL = 8;%16;			% Overlap size which should be even
+OL = 16;			% Overlap size which should be even
 T = 32;%100;		% Template size which should be even
 cand = 20;			% Number of candidate pattern which the final pattern is selected from
 fc = 0;				% 0: no need for facies matching. Otherwise, the facies proportion should be provided (e.g. for a 2 facies TI, this matrix should be like [0.28 0.72])
@@ -47,14 +47,16 @@ T_vibration = 1;	% Multiple template size flag (1:on, 0:off)
 ms_level = 3;		% Number of MS level (1, 2, 3).
 prop = 0.1;			% The proportion of TI which will be scanned to find the matched hard data (0 < prop <=1)). Larger cofficient helps the hard data to honred, but it may cause some discountinuties.
 rad = 1;			% Neighbourhood radius for MS simulation (default or the best radius is [1 1])
-real_numb = 20;		% Number of realizations
+real_numb = 2;		% Number of realizations
 
+use_mincut = 1;
 
 % Main Function
 [C_2D, location_2D] = CCSIM_2D(ti, hd, T, OL, CT, fc, prop, rad, cand, mrp, T_vibration, ms_level, real_numb);
 
 % path = 'C:\Users\gfarina\Desktop\Realizations\WildCat\';
-path = 'C:\Users\gfarina\Desktop\Realizations\AWild\';
+% path = 'C:\Users\gfarina\Desktop\Realizations\AWild\';
+path = 'C:\Users\gfarina\Desktop\Realizations\realizations for the article 2\';
 if ~(exist(path, 'dir') == 7)
 	mkdir(path);
 end
@@ -64,7 +66,7 @@ for i = 1:real_numb % for each realization:
  
  % Normalizing the values of B, to get the GRAY tones!!!
  maxInMatrix = max(b(:));
- b(:,:) = b(:,:)/2;
+ b(:,:) = b(:,:)/maxInMatrix;
  % finished normalizing.
  
  imwrite(b, [path 'MATRIX_' int2str(i) '.jpg']); % and write it down to the JPG
